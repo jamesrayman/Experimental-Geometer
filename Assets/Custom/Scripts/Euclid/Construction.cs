65fe0001d8109a763f8d6ab683cce3140a77e749
Aaron Lamoreaux <LamoreauxAJ@gmail.com>
2019-02-17 07:23:32 -0600

    update point modifying

--- Assets/Custom/Scripts/Euclid/Construction.cs
+++ Assets/Custom/Scripts/Euclid/Construction.cs
@@ -57,6 +57,7 @@ namespace Euclid {
                     if (fig.properties.ContainsKey("render")) {
                         if (Mathf.Approximately((float) fig.properties["render"], 1)) {
                             render.Add(fig);
+                            fig.properties["name"] = variable;
                         }
                     }
                 }
@@ -75,7 +76,7 @@ namespace Euclid {
                 Statement s = root.block.children[i];
                 if (s is AssignmentStatement) {
                     AssignmentStatement assign = s as AssignmentStatement;
-                    if (assign.variables == variables && assign.expression is FunctionExpression) {
+                    if (assign.variables.Count == 1 && assign.variables[0] == variables[0] && assign.expression is FunctionExpression) {
                         FunctionExpression func = assign.expression as FunctionExpression;
                         if (func.name == "point") {
                             root.block.children[i] = new AssignmentStatement(variables, value);
